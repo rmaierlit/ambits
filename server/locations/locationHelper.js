cd65192dd9386613f3484ec0a6607f0ed8cf210f
Robert Littlejohn <bbblittlejohn@gmail.com>
2016-12-07 17:53:36 -0800

    added helper function for check-in validation
    also properly removed location files from the main directory

--- server/locations/locationHelper.js
+++ server/locations/locationHelper.js
@@ -35,3 +35,24 @@ module.exports.addLocation = function (req, res, next) {
       next(error);
     });
 };
+
+module.exports.validateCheckIn = function(req, res, next) {
+  //given a particular location name, check if the geodata for the users
+  //current location matches that location
+  //if the user is trying to check in for a Gym task,
+  //but they are not in the gym, the server responds with a message
+  //if the user is at the gym, the server goes to the next .then statement
+
+  var name = req.body.name;
+  var userGeodata = req.body.geodata;
+
+  findLocation({name: name})
+    .then(function(taskLocation) {
+      if (taskLocation.geodata === userGeodata) {
+        next(location);
+      } else {
+        res.send('No Cheating! You are not at the right location');
+        //replace this if we need send a different validation refusal
+      }
+    });
+};
