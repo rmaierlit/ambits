790567d7443f5a2c36b0e976476b0f5c71708288
Robert Littlejohn <bbblittlejohn@gmail.com>
2016-12-07 16:29:44 -0800

    creates helpers for accessing users in db
    addLocation will create a new location and save it in the database.
    also provided additional comments to locationSchema

--- locationSchema.js
+++ locationSchema.js
@@ -3,8 +3,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var locationSchema = new Schema({
-  name: String,
-  geodata: Number
+  name: String, //e.g. 'Gym', 'Library' (for the user);
+  geodata: Number //replace with whatever we use to store this location
+  //radius (should it be fixed or should it vary?)
   //user for objectId
   //description for String
 });
