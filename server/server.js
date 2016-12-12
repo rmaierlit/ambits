81b376dff323c56149a2c8e518464fb79f8cbacd
Robert Littlejohn <bbblittlejohn@gmail.com>
2016-12-11 19:34:55 -0800

    modified check-in route

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ require('./config/passport');
 if (process.env.NODE_ENV !== 'production') {
   const webpack = require('webpack');
   const webpackDevMiddleware = require('webpack-dev-middleware');
-  // const webpackHotMiddleware = require('webpack-hot-middleware');  
+  // const webpackHotMiddleware = require('webpack-hot-middleware');
   const config = require('../webpack-dev-server.config.js');
   const compiler = webpack(config);
 
@@ -70,7 +70,7 @@ app.set('view engine', 'html');
 app.get('/ambits', ambitHelper.getAmbits);
 app.post('/ambits', ambitHelper.addAmbit);
 
-app.put('/ambits/:id', ambitHelper.saveCheckin);
+app.put('/ambits/:id', ambitHelper.saveCheckIn);
 
 app.post('/register', ctrlAuth.register);
 app.post('/login', ctrlAuth.login);
