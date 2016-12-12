5990b478dab9280cc76304093beaad5622294a12
Robert Littlejohn <bbblittlejohn@gmail.com>
2016-12-12 15:25:07 -0800

    display mockup

--- client/src/app/displayAmbit/displayAmbit.jsx
+++ client/src/app/displayAmbit/displayAmbit.jsx
@@ -7,28 +7,35 @@ export default class displayAmbit extends React.Component {
     super(props);
     this.state  = {
       today: new Date(),
-      ambit: {weekdays: [false,true,false,true,false,true,false]}
+
+      //hardcoded data for mock-up purposes
+      ambit: {weekdays: [true,true,false,true,false,true,true]}, //mon, wed, fri, sat, sun
+      log: {"Sat Dec 10 2016": true, "Mon Dec 12 2016": true} 
     }
   }
 
   dateInLog(date){
-    return false;
+    if (this.state.log[date.toDateString()]){
+      return true;
+    } else {
+      return false;
+    }
   }
 
   render() {
-    console.log('today', this.state.today);
     var startDate = new Date(this.state.today.toDateString());
-    console.log(startDate);
+    startDate.setDate(startDate.getDate() - 3);
     var dates = [];
     for (var i = 0; i < 7; i++){
       var itemInfo = {};
-      var itemDate = startDate.setDate(startDate.getDate() + 1);
-      console.log(itemDate);
+      var itemDate = new Date(startDate);
+      itemDate.setDate(itemDate.getDate() + i);
 
       itemInfo.date = itemDate;
 
       //if this list item's day of the week doesn't match ambit.weekdays
-      if(this.state.ambit.weekdays[itemDate.getDay] === false){
+      console.log(itemDate.getDay);
+      if(this.state.ambit.weekdays[itemDate.getDay()] === false){
         itemInfo.status = 'Free';
       }
       else{
@@ -50,7 +57,7 @@ export default class displayAmbit extends React.Component {
     }
     var listItems = dates.map(function(info){
       return(
-        <ListItem primaryText={info.status} secondaryText={info.date}/>
+        <ListItem primaryText={info.status} secondaryText={info.date.toDateString()}/>
       )
     });
 
